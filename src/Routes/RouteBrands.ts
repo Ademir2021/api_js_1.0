f468cf88d60273338feecdea8895461ad1024c49
Ademir2021 <centroserra@gmail.com>
2024-06-06 19:44:07 -0300

    up-code

--- src/Routes/RouteBrands.ts
+++ src/Routes/RouteBrands.ts
@@ -4,8 +4,6 @@ import { BrandsControllers } from "../Controllers/Brands/BrandsControlers";
 const routeBrands = Router();
 const brandsControllers = new BrandsControllers()
 
-
 routeBrands.get('/brands', brandsControllers.findAll)
-
 
 export { routeBrands }
