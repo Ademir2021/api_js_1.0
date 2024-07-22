e170dfdfc78d54ef05c7b5a9a461785180094b18
Ademir2021 <centroserra@gmail.com>
2024-07-22 11:28:08 -0300

    up-code

--- src/Entities/Sale/SaleDAO.ts
+++ src/Entities/Sale/SaleDAO.ts
@@ -10,7 +10,7 @@ class SaleDAO extends DAO {
 
     async insert(Sales: ISale) {
         try {
-          /*  await postgreSQL.query('INSERT INTO ' + SaleDAO.table + '(fk_name_pers, disc_sale, fk_name_filial, fk_name_user) VALUES ('
+            await postgreSQL.query('INSERT INTO ' + SaleDAO.table + '(fk_name_pers, disc_sale, fk_name_filial, fk_name_user) VALUES ('
                 + "'"
                 + Sales.fkPerson
                 + "','"
@@ -19,10 +19,10 @@ class SaleDAO extends DAO {
                 + Sales.fkFilial
                 + "','"
                 + Sales.fkUserId
-                + "')") */
+                + "')")
             const num_sale_ = await postgreSQL.query("SELECT MAX(id_sale) FROM " + SaleDAO.table + "");
             const num_sale: number = num_sale_.rows[0].max;
-            /*
+
             if (Sales.itens)
                 for (let i = 0; Sales.itens.length > i; i++) {
                     const sum_total_item: number = 0;
@@ -38,10 +38,10 @@ class SaleDAO extends DAO {
                         + "','"
                         + sum_total_item
                         + "')")
-                }; */
+                };
             if (Sales.contasReceber)
                 for (let i = 0; Sales.contasReceber.length > i; i++) {
-                    await postgreSQL.query('INSERT INTO ' + SaleDAO.tableContasReceber + '(fk_filial, tipo, fk_venda, fk_user, parcela, valor, multa, juros, desconto, emissao, vencimento, saldo, pagamento, recebimento) VALUES ('
+                    await postgreSQL.query('INSERT INTO ' + SaleDAO.tableContasReceber + '(fk_filial, tipo, fk_venda, fk_user, parcela, valor, multa, juros, desconto, emissao, vencimento, saldo, recebimento) VALUES ('
                         + "'"
                         + Sales.contasReceber[i].fk_filial
                         + "','"
@@ -67,15 +67,12 @@ class SaleDAO extends DAO {
                         + "','"
                         + Sales.contasReceber[i].saldo
                         + "','"
-                        + Sales.contasReceber[i].pagamento
-                        + "','"
                         + Sales.contasReceber[i].recebimento
                         + "')")
                 };
             return (num_sale)
         } catch (err) {
-            console.log(err)
-            return new SaleDAO().errors(Sales.contasReceber);
+            return new SaleDAO().errors(err);
         }
     };
 }
