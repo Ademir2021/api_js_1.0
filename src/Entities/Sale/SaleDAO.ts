742d13996ec2a05f2e591ee89002cdff5642897f
Ademir2021 <centroserra@gmail.com>
2024-07-29 01:04:56 -0300

    up-code

--- src/Entities/Sale/SaleDAO.ts
+++ src/Entities/Sale/SaleDAO.ts
@@ -25,34 +25,34 @@ class SaleDAO extends DAO {
             const num_sale: number = num_sale_.rows[0].max;
 
             if (Sales.itens)
-                for (let Sale of Sales.itens) {
+                for (let item of Sales.itens) {
                     const sum_total_item: number = 0;
                     await postgreSQL.query('INSERT INTO ' + SaleDAO.tableItens + '(fk_sale, fk_product, amount_product, val_product, total_product) VALUES ('
                         + "'" + num_sale + "','"
-                        + Sale.item + "','"
-                        + Sale.amount + "','"
-                        + Sale.valor + "','"
+                        + item.item + "','"
+                        + item.amount + "','"
+                        + item.valor + "','"
                         + sum_total_item
                         + "')")
                 };
 
             if (Sales.contasReceber)
-                for (let Sale of Sales.contasReceber) {
+                for (let conta of Sales.contasReceber) {
                     await postgreSQL.query('INSERT INTO ' + SaleDAO.tableContasReceber + '(fk_filial, tipo, fk_venda, fk_user, parcela, valor, multa, juros, desconto, emissao, vencimento, saldo, recebimento) VALUES ('
                         + "'"
-                        + Sale.fk_filial + "','"
-                        + Sale.tipo + "','"
+                        + conta.fk_filial + "','"
+                        + conta.tipo + "','"
                         + num_sale + "','"
-                        + Sale.fk_user + "','"
-                        + Sale.parcela + "','"
-                        + Sale.valor + "','"
-                        + Sale.multa + "','"
-                        + Sale.juros + "','"
-                        + Sale.desconto + "','"
-                        + Sale.emissao + "','"
-                        + Sale.vencimento + "','"
-                        + Sale.saldo + "','"
-                        + Sale.recebimento
+                        + conta.fk_user + "','"
+                        + conta.parcela + "','"
+                        + conta.valor + "','"
+                        + conta.multa + "','"
+                        + conta.juros + "','"
+                        + conta.desconto + "','"
+                        + conta.emissao + "','"
+                        + conta.vencimento + "','"
+                        + conta.saldo + "','"
+                        + conta.recebimento
                         + "')")
                 };
             return (num_sale)
