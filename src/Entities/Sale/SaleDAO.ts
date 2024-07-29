4283e0f8e5dc40a60a70187f6db6e2a13e203aba
Ademir2021 <centroserra@gmail.com>
2024-07-29 00:50:34 -0300

    up-code

--- src/Entities/Sale/SaleDAO.ts
+++ src/Entities/Sale/SaleDAO.ts
@@ -20,54 +20,39 @@ class SaleDAO extends DAO {
                 + "','"
                 + Sales.fkUserId
                 + "')")
+
             const num_sale_ = await postgreSQL.query("SELECT MAX(id_sale) FROM " + SaleDAO.table + "");
             const num_sale: number = num_sale_.rows[0].max;
 
             if (Sales.itens)
-                for (let i = 0; Sales.itens.length > i; i++) {
+                for (let Sale of Sales.itens) {
                     const sum_total_item: number = 0;
                     await postgreSQL.query('INSERT INTO ' + SaleDAO.tableItens + '(fk_sale, fk_product, amount_product, val_product, total_product) VALUES ('
-                        + "'"
-                        + num_sale
-                        + "','"
-                        + Sales.itens[i].item
-                        + "','"
-                        + Sales.itens[i].amount
-                        + "','"
-                        + Sales.itens[i].valor
-                        + "','"
+                        + "'" + num_sale + "','"
+                        + Sale.item + "','"
+                        + Sale.amount + "','"
+                        + Sale.valor + "','"
                         + sum_total_item
                         + "')")
                 };
+
             if (Sales.contasReceber)
-                for (let i = 0; Sales.contasReceber.length > i; i++) {
+                for (let Sale of Sales.contasReceber) {
                     await postgreSQL.query('INSERT INTO ' + SaleDAO.tableContasReceber + '(fk_filial, tipo, fk_venda, fk_user, parcela, valor, multa, juros, desconto, emissao, vencimento, saldo, recebimento) VALUES ('
                         + "'"
-                        + Sales.contasReceber[i].fk_filial
-                        + "','"
-                        + Sales.contasReceber[i].tipo
-                        + "','"
-                        + num_sale
-                        + "','"
-                        + Sales.contasReceber[i].fk_user
-                        + "','"
-                        + Sales.contasReceber[i].parcela
-                        + "','"
-                        + Sales.contasReceber[i].valor
-                        + "','"
-                        + Sales.contasReceber[i].multa
-                        + "','"
-                        + Sales.contasReceber[i].juros
-                        + "','"
-                        + Sales.contasReceber[i].desconto
-                        + "','"
-                        + Sales.contasReceber[i].emissao
-                        + "','"
-                        + Sales.contasReceber[i].vencimento
-                        + "','"
-                        + Sales.contasReceber[i].saldo
-                        + "','"
-                        + Sales.contasReceber[i].recebimento
+                        + Sale.fk_filial + "','"
+                        + Sale.tipo + "','"
+                        + num_sale + "','"
+                        + Sale.fk_user + "','"
+                        + Sale.parcela + "','"
+                        + Sale.valor + "','"
+                        + Sale.multa + "','"
+                        + Sale.juros + "','"
+                        + Sale.desconto + "','"
+                        + Sale.emissao + "','"
+                        + Sale.vencimento + "','"
+                        + Sale.saldo + "','"
+                        + Sale.recebimento
                         + "')")
                 };
             return (num_sale)
